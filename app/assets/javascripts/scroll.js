02e3b3a221892980d4a45cefbfcead824119735c
mattadam <mattnery@gmail.com>
2014-06-22 19:48:52 -0600

    link changes

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function($) {
+	$("#overview").click(function() {
+		$('html, body').animate({
+	        scrollTop: $("#who-we-are").offset().top -100 
+	    }, 1000);
+		event.preventDefault();
+	});
+	$("#river-deep-section").click(function() {
+		$('html, body').animate({
+	        scrollTop: $("#river-deep").offset().top -100 
+	    }, 1000);
+		event.preventDefault();
+	});
+	$("#fishery-section").click(function() {
+		$('html, body').animate({
+	        scrollTop: $("#fishery").offset().top -10
+	    }, 1000);
+		event.preventDefault();
+	});
+	$("#overview-tab").click(function() {
+		event.preventDefault();
+	});
+});
