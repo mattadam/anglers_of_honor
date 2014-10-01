6466ee96fad6495bd376c177a6db192252d7c3af
mattadam <mattnery@gmail.com>
2014-09-30 19:46:18 -0600

    GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  GA.tracker = "UA-55323310-1"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
